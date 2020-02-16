527e10943e904c3934ca11108c38d5f44fb4d3cf
Avrahm Kleinholz <me@avrahm.com>
2020-02-15 23:53:54 -0500

    randomize three workouts

--- src/App.js
+++ src/App.js
@@ -1,6 +1,5 @@
 import React from 'react';
 import './App.css';
-import { render } from '@testing-library/react';
 
 class App extends React.Component {
 
@@ -9,14 +8,44 @@ state = {
   workoutResults: '',
 }
 
+pushups
+squats
+jumprope
+
 chooseWorkout = () => {
 
-  let randomWorkout = this.state.workOuts[Math.floor(Math.random()*this.state.workOuts.length)]
+
+  let workoutArr = [];
+  // let randomNum = Math.floor(Math.random()*this.state.workOuts.length);
+  let randomWorkout1 = this.state.workOuts[Math.floor(Math.random()*this.state.workOuts.length)];
+  let randomWorkout2 = this.state.workOuts[Math.floor(Math.random()*this.state.workOuts.length)];
+  let randomWorkout3 = this.state.workOuts[Math.floor(Math.random()*this.state.workOuts.length)];
+  
+  workoutArr.push(randomWorkout1, randomWorkout2, randomWorkout3);
+
+  this.state.workOuts.splice(this.state.workOuts.indexOf(randomWorkout1), 1)
+  this.state.workOuts.splice(this.state.workOuts.indexOf(randomWorkout2), 1)
+  this.state.workOuts.splice(this.state.workOuts.indexOf(randomWorkout3), 1)
+  
+  console.log(this.state.workOuts)
+  console.log('workoutArr '+workoutArr)
+
+  
+  let threeWorkouts = workoutArr.map((eachWorkout, index) => {
+    return  (
+    <ul key={index}>
+      <li >{eachWorkout}</li>
+    </ul>
+      );
+  })
 
   this.setState({
-    workoutResults: randomWorkout
+
+    workoutResults: threeWorkouts,
+    workOuts: [ 'pushups', 'abs', 'squats', 'jumprope intervals', 'jumping lunges', 'burpees', 'lunges', 'body rows', 'sprint intervals', 'pullups', 'plyometric pushups'  ],
+
   })
-  
+
 }
   
   render(){
@@ -26,9 +55,9 @@ chooseWorkout = () => {
 
       <button onClick={this.chooseWorkout}>Random Workout</button>
 
-      <p style={{padding: '10px', backgroundColor: 'blue', width: '200px', height: '200px', textAlign: 'center'}}>
+      <div style={{padding: '10px', backgroundColor: 'blue', width: '200px', height: '200px', textAlign: 'center'}}>
       {this.state.workoutResults}
-      </p>
+      </div>
         
       </header>
     </div>
